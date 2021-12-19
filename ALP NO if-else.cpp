f9cc6d59dfd615cb661914d51ca2afecaaeb0751
lokesh-reddy14 <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-19 14:54:59 +0530

    Create ALP NO if-else.cpp

---
+++ ALP NO if-else.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{ 
+	int alp;
+	scanf("%d",&alp);
+	if((alp>=65) && (alp<=90))
+	{ 
+	 printf("captial");
+	 
+	}
+	else if((alp>=97) && (alp<=122))
+	{ 
+		printf("small");
+	}
+	else
+	
+	{ 
+		printf("special character");
+	}
+}
