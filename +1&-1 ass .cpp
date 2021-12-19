bc30d644c144afadfb960cd701e2da36765c81c1
lokesh-reddy14 <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-19 14:41:06 +0530

    Add files via upload

---
+++ +1&-1 ass .cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    
+    scanf("%d",&num);
+    if (num>0)
+	{ 
+	 printf("1");
+	 } 
+	else if(num<0)
+	{ 
+	 printf("-1");
+	}
+	else
+	{ 
+	 printf("0");
+	}
+}
+
