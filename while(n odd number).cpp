f9fb11edda1b4626d4a4f9ef40976a6268eb2205
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 15:28:14 +0530

    Update and rename case(positive nor negative).cpp to while(n odd number).cpp

---
+++ while(n odd number).cpp
@@ -0,0 +1,18 @@
+/*3. Given an integer N, print all the odd numbers from 1 to N in ascending order.*/
+
+#include<stdio.h>
+int main()
+{ 
+int i,n,b;
+scanf("%d",&n);
+i=1;
+
+while(i<=n)
+{
+
+{if (i%2!=0)
+printf("%d\n",i);
+}
+ i=i+1;
+}
+}
