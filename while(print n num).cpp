0b291ce73cb59713215e74e81925ef61c8247d0e
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 15:23:45 +0530

    Create while(print n num).cpp

---
+++ while(print n num).cpp
@@ -0,0 +1,14 @@
+/*1. Given an integer N, print all the numbers from 1 to N.*/
+
+#include<stdio.h>
+int main()
+{ 
+int i,n;
+scanf("%d",&n);
+i=1;
+ while(i<=n)
+ { 
+ printf("%d\n",i);
+ i=i+1;
+ }
+}
