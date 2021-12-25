9b22267280b9f11c48db2f5d5534fd93f658b2bb
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 14:38:01 +0530

    Create print(relational opperators).cpp

---
+++ print(relational opperators).cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter the values of a=");
+	scanf("%d",&a);
+	printf("enter the value of b=");
+	scanf("%d",&b);
+	printf("a<b=""%d\n",a<b);
+	printf("a>b=""%d\n",a>b);
+	printf("a<=b=""%d\n",a<=b);
+	printf("a>=b=""%d\n",a<=b);
+	printf("a!=b=""%d\n",a!=b);
+	printf("a==b=""%d",a==b);
+}
