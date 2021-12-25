b9ce831d6e8176056cb1a077b7ef4467f37930f1
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 15:17:14 +0530

    Create if-else(ovel or constant).cpp

---
+++ if-else(ovel or constant).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char op;
+	scanf("%c",&op);
+	if((op=='a')||(op=='e')||(op=='i')||(op=='u')||op==('o'))
+	 { printf("it is vowel");
+	 }
+	else
+	{ printf("it is consontant");
+	}
+}
