f29945e8639e56045f62f08d0a6e6cb0563419f4
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 14:58:25 +0530

    Create switch(day).cpp

---
+++ switch(day).cpp
@@ -0,0 +1,31 @@
+/*1. Write a C program to print day of week name using switch case.*/
+#include<stdio.h>
+int main()
+{ int num;
+scanf("%d",&num);
+	switch(num)
+	{  case 1:
+			printf("sunday");
+			break;
+		case 2:
+			printf("monday");
+			break;
+		case 3:
+			printf("tuesday");
+			break;
+		case 4:
+			printf("wednesday");
+			break;
+		case 5:
+			printf("thursday");
+			break;
+		case 6:
+			printf("friday");
+			break;
+		case 7:
+			printf("saturday");
+			break;
+		default:
+			printf("no day exits");
+	}
+}
