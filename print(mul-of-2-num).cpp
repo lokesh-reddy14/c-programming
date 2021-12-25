b7ce4fafd300b941d81b85388cdf0dbc9c6b5561
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 14:20:12 +0530

    Create print(mul-of-2-num).cpp

---
+++ print(mul-of-2-num).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n1,n2,n3;
+  printf("enter the number1:");
+  scanf("%d",&n1);
+  printf("enter the number2:");
+  scanf("%d",& n2);
+  n3=n1*n2;
+  printf("the product of two numbers is %d",n3);
+  return(0);
+}
