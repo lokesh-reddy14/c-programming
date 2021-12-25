d515b1c8835aa13930bdfdece80db0b2ec8f4590
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 13:03:58 +0530

    Create captal or small by num.cpp

---
+++ captal or small by num.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+
+{ 
+	int alp;
+	scanf("%d",&alp);
+	if((alp>=65) && (alp<=90))
+	{ 
+	 printf("captial");
+	 
+	}
+	else if((alp>=97) && (alp<=122))
+	{ 
+		printf("small");
+	}
+	else
+	
+	{ 
+		printf("special character");
+	}
+}
+© 2021 GitHub, Inc.
+Terms
