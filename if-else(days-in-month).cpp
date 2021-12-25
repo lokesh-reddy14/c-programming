438b8c2ae99b10c8e3f9e29bebcacf9b8c9a4260
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 14:17:05 +0530

    Create if-else(days-in-month).cpp

---
+++ if-else(days-in-month).cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int main()
+
+{ 
+	int i;
+	scanf("%d",&i);
+	if((i==1))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==2))
+	{ 
+	 printf("28");
+	 
+	}
+	else if((i==3))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==4))
+	{ 
+	 printf("30");
+	 
+	}
+	else if((i==5))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==6))
+	{ 
+	 printf("30");
+	 
+	}
+	else if((i==7))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==8))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==9))
+	{ 
+	 printf("30");
+	 
+	}
+	else if((i==10))
+	{ 
+	 printf("31");
+	 
+	}
+	else if((i==11))
+	{ 
+	 printf("30");
+	 
+	}
+	else if((i==12))
+	{ 
+	 printf("31");
+	 
+	}
+
+
+}
