0ef11ce15cf5f9b6522889fc89a75aac68300ec5
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 15:19:23 +0530

    Create while(print n even num).cpp

---
+++ while(print n even num).cpp
@@ -0,0 +1,18 @@
+/*5. Given an integer N, print all the even numbers from 0 to N in descending order*/
+
+ #include<stdio.h>
+int main()
+{ 
+int i,n;
+scanf("%d",&n);
+i=0;
+
+while(i<=n)
+{
+
+{if (n%2==0)
+printf("%d\n",n);
+}
+ n=n-1;
+}
+}
