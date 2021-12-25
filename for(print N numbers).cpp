f01896d04177b20c1de8a659585177db01e3033b
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 13:53:55 +0530

    Create for(print N numbers).cpp

---
+++ for(print N numbers).cpp
@@ -0,0 +1,14 @@
+/*1. Given an integer N, print all the numbers from 1 to N.*/
+
+#include<stdio.h>
+int main()
+{ 
+int i,n;
+scanf("%d",&n);
+i=1;
+ for(i=1;i<=n;i++)
+ { 
+ printf("%d\n",i);
+ 	
+ }
+}
