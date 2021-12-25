ade7deaaf581305b5f095e8827575c094eebc702
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 15:09:06 +0530

    Update and rename switch(simple calculator).cpp to switch(ovel or constant).cpp

---
+++ switch(ovel or constant).cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main ()
+{
+    char op;
+    scanf("%c",&op);
+    switch(op)
+    { case 'a':
+        printf("vowel");
+        break;
+    case 'e':
+        printf("vowel");
+        break;
+    case 'i':
+        printf("vowel");
+        break;
+       case 'o':
+        printf("vowel");
+        break;
+    case 'u':
+        printf("vowel");
+        break;
+    case 'A':
+        printf("vowel");
+        break;
+    case 'E':
+        printf("vowel");
+        break;    
+    case 'O':
+        printf("vowel");
+        break;
+    case 'I':
+        printf("vowel");
+        break;
+    case 'U':
+        printf("vowel");
+        break;    
+    default:
+        printf(" consonant");
+        break;
+    }    
+}
