ef01d9852d7725df34476a713e2cc8a1f08e93c3
LOKESH REDDY <96257635+lokesh-reddy14@users.noreply.github.com>
2021-12-25 13:25:32 +0530

    Create even(or)odd conditional.cpp

---
+++ even(or)odd conditional.cpp
@@ -0,0 +1,9 @@
+/*3. Write a C program to check whether a number is even or odd using conditional operator.*/
+
+#include<stdio.h>
+int main()
+{  
+int x;
+ scanf("%d",&x);
+ (x%2==0)?printf("even"):printf("odd");
+}
